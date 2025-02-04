67ff0dc01cf3539f164e307928e129e0c332836d
Your Name <himanshigiri01@gmail.com>
2025-02-05 00:13:31 +0530

    reinitialized repo with secure API setupt

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ChatBox from "./components/ChatBox";
+import "./styles/App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <ChatBox />
+    </div>
+  );
+};
+
+export default App;
